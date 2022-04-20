d2f2b45bf142f3c493660df5f112a78818562839
Egor Khomyakov <egor-khomyakov@mail.ru>
2022-04-20 12:09:13 +0300

    TestChanges

--- src/Client.java
+++ src/Client.java
@@ -3,21 +3,27 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
+    public void runPrintChat () throws IOException {
         Socket clientSocket = new Socket("127.0.0.1", 48648);
         Scanner scan = new Scanner(System.in);
         OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(
                         clientSocket.getInputStream()));
-            String message = scan.nextLine();
-            writer.write("Send: " + message + "\n");
-            writer.flush();
+        String message = scan.nextLine();
+        writer.write("Send: " + message + "\n");
+        writer.flush();
 
-            reader.close();
-            writer.close();
-            clientSocket.close();
+        reader.close();
+        writer.close();
+        clientSocket.close();
 
     }
 
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            Client client = new Client();
+            client.runPrintChat();
+        }
+    }
 }
--- src/Client2.java
+++ src/Client2.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Client2 {
-    public static void main(String[] args) throws IOException {
+    public void getMeChat() throws IOException {
         Socket clientSocket = new Socket("127.0.0.1", 48648);
         Scanner scan = new Scanner(System.in);
         OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
@@ -14,16 +14,25 @@ public class Client2 {
         writer.write("Get" + "\n");
         writer.flush();
 
+        System.out.print("\033[H\033[J");
+        System.out.flush();
+
         int chatSize = reader.read();
         for (int i = 0; i < chatSize; i++) {
-            String string = reader.readLine() + "\n";
+            String string = reader.readLine();
             System.out.println(string);
         }
 
         writer.close();
         reader.close();
         clientSocket.close();
+    }
 
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            Client2 client2 = new Client2();
+            client2.getMeChat();
+        }
     }
 
 }
